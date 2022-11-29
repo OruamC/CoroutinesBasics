9e365998f6035e652b2370865a702f6166d44c24
Oruam Motta <62652819+OruamC@users.noreply.github.com>
2022-11-29 18:27:23 -0300

    Coroutine context

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,24 +1,12 @@
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 fun main() {
-    println("Program execution will now block")
     runBlocking {
-        launch {
-            delay(1000L)
-            println("Task from runBlocking")
-        }
-
-        GlobalScope.launch {
-            delay(500L)
-            println("Task from GlobalScope")
-        }
-
-        coroutineScope {
-            launch {
-                delay(1500L)
-                println("Task from coroutineScope")
-            }
+        launch(CoroutineName("myCoroutine")) {
+            println("This is run from ${coroutineContext[CoroutineName.Key]}")
         }
     }
-    println("Program execution will now continue")
 }
