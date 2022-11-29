1d07365901fdbd81f8fd847722360b0c7cb809fb
Oruam Motta <62652819+OruamC@users.noreply.github.com>
2022-11-29 19:02:47 -0300

    Using dispatchers

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,39 +1,27 @@
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-
-var functionCalls = 0
+import kotlinx.coroutines.*
 
 fun main() {
     runBlocking {
-        val job1 = launch {
-//            delay(3000L)
-            println("Job1 launched")
-            val job2 = launch {
-                println("Job2 launched")
-                delay(3000L)
-                println("Job2 is finishing")
-            }
-            job2.invokeOnCompletion {
-                println("Job2 completed")
-            }
+//        launch(Dispatchers.Main) {
+//            println("Main dispatcher. Thread ${Thread.currentThread().name}")
+//        }
 
-            val job3 = launch {
-                println("Job3 launched")
-                delay(3000L)
-                println("Job3 is finishing")
-            }
-            job3.invokeOnCompletion {
-                println("Job3 completed")
-            }
+        launch(Dispatchers.Unconfined) {
+            println("Unconfined1. Thread ${Thread.currentThread().name}")
+            delay(100L)
+            println("Unconfined2. Thread ${Thread.currentThread().name}")
         }
 
-        job1.invokeOnCompletion {
-            println("Job1 completed")
+        launch(Dispatchers.Default) {
+            println("Default dispatcher. Thread ${Thread.currentThread().name}")
         }
-        delay(500L)
 
-        println("Job1 will be cancelled")
-        job1.cancel()
+        launch(Dispatchers.IO) {
+            println("IO dispatcher. Thread ${Thread.currentThread().name}")
+        }
+
+        launch(newSingleThreadContext("MyThread")) {
+            println("NewSingleThreadContext. Thread ${Thread.currentThread().name}")
+        }
     }
 }
