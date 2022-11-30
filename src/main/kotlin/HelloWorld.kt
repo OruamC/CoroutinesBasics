1dfd5810abeeea5b38adb2a3c8d59a4e631020b3
Oruam Motta <62652819+OruamC@users.noreply.github.com>
2022-11-30 17:15:13 -0300

    using with context

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,34 +1,16 @@
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlin.random.Random
+import kotlinx.coroutines.withContext
 
 fun main() {
     runBlocking {
-        val firstDeferred = async { getFirstValue() }
-        val secondDeferred = async { getSecondValue() }
-
-        println("Doing some processing here")
-        delay(500L)
-        println("Waiting for values")
-
-        val firstValue = firstDeferred.await()
-        val secondValue = secondDeferred.await()
-
-        println("The total is ${firstValue + secondValue}")
+        launch(Dispatchers.Default) {
+            println("First context: $coroutineContext")
+            withContext(Dispatchers.IO) {
+                println("Second context: $coroutineContext")
+            }
+            println("Third context: $coroutineContext")
+        }
     }
-}
-
-suspend fun getFirstValue(): Int {
-    delay(1000L)
-    val value = Random.nextInt(100)
-    println("Returning first value $value")
-    return value
-}
-
-suspend fun getSecondValue(): Int {
-    delay(2000L)
-    val value = Random.nextInt(1000)
-    println("Returning second value $value")
-    return value
 }
